52eb3d47cc7dee57d5bb04eccd4766c6459c0671
Adhish Singla <adhisingla94@gmail.com>
2017-08-04 23:28:10 +0530

    Initial Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './navigationBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavigationBar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ src/navigationBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">Adhish Singla</a>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    );
+  }
+}
+
+export default NavigationBar;
