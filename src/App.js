390f5cd592f85eeba91278a1318480b6e2a13223
Adhish Singla <adhisingla94@gmail.com>
2017-08-05 00:03:46 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -10,11 +10,8 @@ class App extends Component {
         <NavigationBar />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to my Website</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
